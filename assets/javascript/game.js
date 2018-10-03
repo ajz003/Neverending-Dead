dc7fbed739a71eaa669efd63331d92a12fb29147
TimG <timgm3@gmail.com>
2018-10-03 16:54:18 -0700

    comments and cleaned up code, changed variable names for consistency

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,62 @@
 // initial stat values
 let myAttack = 30;
 let myHealth = 100;
-let killCount = 0;
-
 let enemyHealth = 100;
 let enemyAttack = 10;
 
 
+// hide the gameover and winscreen when the game starts
 $("#gameover").hide();
 $("#winscreen").hide();
 
-// // sets text to initial values
+// sets DOM text to initial values
 $("#myHealth").text(myHealth);
 $("#myAttack").text(myAttack);
-
 $("#enemyHealth").text(enemyHealth);
 $("#enemyAttack").text(enemyAttack);
 
+// Attack logic
 
 
+// Attack button calls attack function
 $("#attackButton").on("click", function () {
 
-    $(".console1").text("You attacked for " + myAttack + " damage!")
-
+    // Reduce enemy health by myAttack value
     enemyHealth -= myAttack;
 
-    $(".myOpp").find('span.charHealth').text(enemyHealth);
-
-
+    // check if enemy died before he counter-attacks
     if (enemyHealth <= 0) {
-        $(".myOpp").remove();
-            $("#gameDiv").hide();
-            $("#winscreen").show();
+
+        // hides the game and shows the winscreen
+        $("#gameDiv").hide();
+        $("#winscreen").show();
     };
 
+    // check if enemy survived my attack
+    if (enemyHealth > 0) {
 
-    if (enemyHealth >= 0) {
+        // reduce my health by enemyAttack
         myHealth -= enemyAttack;
-        $(".console2").text("Your opponent hit back for " + enemyAttack + " damage!")
+
+        // update console lines
+        $(".console1").text("You attacked for " + myAttack + " damage!");
+        $(".console2").text("Your opponent hit back for " + enemyAttack + " damage!");
+
     };
 
-
-
+    // after enemy counter-attack, check if my characer died
     if (myHealth <= 0) {
+
+        // hides the game and shows the gameover screen
         $("#gameDiv").hide();
         $("#gameover").show();
     };
-    $(".myChar").find('span.charHealth').text(myHealth);
-    $(".myChar").find('span.charAttack').text(myAttack);
 
+    // if no one died, update the character stat boxes
+    $('#enemyHealth').text(enemyHealth);
+    $('#myHealth').text(myHealth);
 
 });
-
-
-
-
-
-
 
 
 
